e2766fca72e2686ac401c7970189976ca8e8dc81
Luis Fernando Chimelo Ruiz <ruiz.ch@gmail.com>
2014-06-19 23:54:42 -0300

    Update apenas_atrings.py

--- apenas_atrings.py
+++ apenas_atrings.py
@@ -2,7 +2,7 @@ variavel = raw_input('Entre com o nome: ')
 
 while variavel != 'sair':
     
-    if str(variavel).isdigit():
+    if str(variavel).isalpha() == False:
         print 'Inserir apenas string'
         variavel = raw_input('Entre com o nome: ')
     else:
